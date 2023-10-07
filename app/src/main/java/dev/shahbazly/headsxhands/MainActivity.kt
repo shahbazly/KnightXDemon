ce90a29f2e185205f000522f87c4b78d295be9f4
shahbazly <shahbazly.dev@gmail.com>
2023-10-07 09:10:51 +0300

    minor update

--- app/src/main/java/dev/shahbazly/headsxhands/MainActivity.kt
+++ app/src/main/java/dev/shahbazly/headsxhands/MainActivity.kt
@@ -1,33 +1,39 @@
 package dev.shahbazly.headsxhands
 
-import android.graphics.drawable.AnimationDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var knightAnimation: AnimationDrawable
-    private lateinit var demonAnimation: AnimationDrawable
+    private lateinit var knightImage: ImageView
+    private lateinit var demonImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val knightImage = findViewById<ImageView>(R.id.knightImage).apply {
-            setBackgroundResource(R.drawable.knight_idle_animation)
-            knightAnimation = background as AnimationDrawable
-        }
-        val demonImage = findViewById<ImageView>(R.id.demonImage).apply {
-            setBackgroundResource(R.drawable.demon_idle_animation)
-            demonAnimation = background as AnimationDrawable
+        knightImage = findViewById(R.id.knightImage)
+        demonImage = findViewById(R.id.demonImage)
+
+        val knight = Player(name = "Knight", avatar = knightImage, attack = 100, defense = 10, health = 10, damage = 15)
+        val demon = Monster(name = "Demon", avatar = demonImage, attack = 150, defense = 16, health = 100, damage = 20)
+
+        val demonAttackButton = findViewById<Button>(R.id.demonAttackButton)
+        val knightAttackButton = findViewById<Button>(R.id.knightAttackButton)
+
+        demonAttackButton.setOnClickListener {
+            demon.attack(knight)
+            demonAttackButton.isClickable = false
+            knightAttackButton.isClickable = knight.isAlive()
         }
 
-        startGame()
-    }
+        knightAttackButton.setOnClickListener {
+            knight.attack(demon)
+            knightAttackButton.isClickable = false
+            demonAttackButton.isClickable = demon.isAlive()
+        }
 
-    private fun startGame() {
-        knightAnimation.start()
-        demonAnimation.start()
     }
 }
